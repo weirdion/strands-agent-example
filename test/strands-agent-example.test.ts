9cdc828ae0a8560c8efefa415ba5a57b01138eee
Ankit Patterson <dev@weirdion.com>
2025-07-13 08:55:33 -0400

    Initial cdk commit

---
+++ test/strands-agent-example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StrandsAgentExample from '../lib/strands-agent-example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/strands-agent-example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StrandsAgentExample.StrandsAgentExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
