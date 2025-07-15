018b0ea14fc402759a37ea84d8c568f897e41956
Ankit Patterson <dev@weirdion.com>
2025-07-15 09:04:13 -0400

    Add tools

--- app/agent.py
+++ app/agent.py
@@ -1,10 +1,12 @@
 from aws_lambda_powertools import Logger
 from strands import Agent
+from tools import check_unraid_health
 
 logger = Logger("strands-agent-example")
 
 agent = Agent(
     model="us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    tools=[check_unraid_health],
     system_prompt=(
         "You are a helpful assistant, who is knowledgeable about various topics. "
         "Answer the user's questions to the best of your ability. "
---
+++ app/tools.py
@@ -0,0 +1,31 @@
+from aws_lambda_powertools import Logger
+from strands import tool
+
+logger = Logger("strands-agent-example")
+
+
+@tool(name="check_unraid_health", description="Check the health of the unraid system")
+def check_unraid_health() -> str:
+    """
+    Function to check the health of the unraid system.
+    """
+    # Mock call via VPC endpoint to check health of my unraid system at home.
+    response = {
+        "status": "healthy",
+        "content": [
+            {
+                "drive": "Disk 1",
+                "status": "operational",
+                "temperature": "30C",
+                "errors": 0,
+            },
+            {
+                "drive": "Disk 2",
+                "status": "operational",
+                "temperature": "32C",
+                "errors": 0,
+            },
+        ],
+    }
+    logger.info(f"Unraid health check response: {response}")
+    return response
